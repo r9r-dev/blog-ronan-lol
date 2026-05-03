f1057dd5129522bd38c10f716ca0d373e4902e61
r9r-dev <bold.spot3022@lamour.bzh>
2026-05-04 01:34:11 +0200

    chore: add MDX integration and Vitest

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
+import mdx from '@astrojs/mdx';
 
 export default defineConfig({
   site: 'https://ronan.lol',
   trailingSlash: 'never',
-  integrations: [sitemap()],
+  integrations: [mdx(), sitemap()],
   markdown: {
     shikiConfig: {
       theme: 'rose-pine-moon',
